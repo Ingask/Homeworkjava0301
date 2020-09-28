745dbbbd412eb4a27dd95c86a2719969cf338f1d
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2020-09-28 17:14:39 +0300

    BonusMilesService added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
    public static void main(String[] args) {
 
-       double price = 115.53;
-       int miles =  (int) price /  20;
-
+       BonusMilesService service = new BonusMilesService();
+       int price = 10_000;
+       int miles = service.calculate(142);
        System.out.println(miles);
 
+
+
+
+
+
+
+       // double price = 115.53;
+       // int miles =  (int) price /  20;
+       //System.out.println(miles);
 }
 
 }
