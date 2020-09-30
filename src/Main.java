d7089e6505041047315a47448ed48b36d9eadac1
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2020-09-30 14:06:58 +0300

    Code edited

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
 public class Main {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-       BonusMilesService service = new BonusMilesService();
-       int price = 10_000;
-       int miles = service.calculate(price);
-       System.out.println(miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
-
-
-
-
-
-
-       // double price = 115.53;
-       // int miles =  (int) price /  20;
-       //System.out.println(miles);
-}
+    }
 
 }
